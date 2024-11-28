09e0e593c3bcc2bdcb21096f4852c75ea08edabe
Martin-koder1 <martinmartinkoder@gmail.com>
2024-11-28 13:57:59 +0100

    genderboxes

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const YourAge = document.querySelector('#Age')
 const YourNumber = document.querySelector('#PhoneNumber')
 const YourEmail = document.querySelector('#Email')
 
+const FemaleElement = document.querySelector("#Female")
+const MaleElement = document.querySelector("#Male")
+const OtherElement = document.querySelector("#Other")
+
 function submit(event) {
   event.preventDefault();
   
@@ -12,10 +16,23 @@ function submit(event) {
   const PersonNumber = PhoneNumber.value;
   const PersonEmail = YourEmail.value;
 
+  const GenderArray = [];
+
+  if (FemaleElement.checked === true) {
+    GenderArray.push(FemaleElement.value)
+  }
+  if (MaleElement.checked === true) {
+    GenderArray.push(MaleElement.value)
+  }
+  if (OtherElement.checked === true) {
+    GenderArray.push(OtherElement.value)
+  }
+
   console.log(FullName);
   console.log(PersonAge);
   console.log(PersonNumber);
   console.log(PersonEmail);
+  console.log(GenderArray);
 }
 
 CompleteJob.addEventListener('click', submit);
